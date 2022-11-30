fa2669a89020dfd16846f67781b5b214ca9c24e5
Wilberth <wilberthlp4@gmail.com>
2022-11-30 13:01:49 -0600

    Primer commit

---
+++ sw.js
@@ -0,0 +1,31 @@
+const STATIC_CACHE    = 'static-v2';
+const DYNAMIC_CACHE   = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'css/font-awesome/css/font-awesome.css',
+    'css/font-awesome/css/font-awesome.min.css',
+    'css/font-awesome/font/',
+    'img/favicon.ico',
+    'img/tiendas/aurrera.jpg',
+    'img/tiendas/eleven.jpg',
+    'img/tiendas/oxxo.png',
+    'img/tiendas/soriana.png',
+    'js/app.js',
+    'js/sidebar.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+    'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+    // 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+    'https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49',
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy',
+    'https://cdn.jsdelivr.net/npm/tether@2.0.0-beta.5/dist/js/tether.min.js',
+    'https://cdn.jsdelivr.net/npm/pouchdb@7.2.1/dist/pouchdb.min.js'
+];
